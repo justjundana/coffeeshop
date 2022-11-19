3898b4286c05b6cb104981c5e62bd795300b2540
justjundana <jalbasyir@gmail.com>
2022-11-19 12:59:18 +0700

    Initial Commit

---
+++ coffeeshop/coffeeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeshopApp.swift
+//  coffeeshop
+//
+//  Created by Jundana Al-Basyir on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct coffeeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
